2757abe468adb8cdb57166e98b4c7d26bb5cfe43
Anton <132255178+dxfh00@users.noreply.github.com>
2024-08-12 09:24:25 +0300

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -1,8 +1,17 @@
-def print_params(a, b, c):
+def print_params(a=1, b='строка', c=True):
     print(a, b, c)
 
 
 values_list = (9, True, 'str')
 values_dict = {'a': 11, 'b': 22, 'c': 33}
 values_list2 = (54.32, 'str')
+
+print_params()
+print_params(a='str', c=False)
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+print_params(*values_list)
+print_params(**values_dict)
+
 print_params(*values_list2, 42)
